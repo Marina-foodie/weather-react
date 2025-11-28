bdb62f19d15cf7b92a04d96793f9c40bd68a3893
Marina-foodie <marina.ortner@gmail.com>
2025-11-28 22:03:20 +0100

    Cleanup: removed build and node_modules, updated source files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./App.css";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
